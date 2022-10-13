e57475dd993ee2c2ad1fbdcdb93db47e5d42e1bd
Tadokiari <78386551+Rielaph@users.noreply.github.com>
2022-10-13 18:29:55 -0400

    Anime Frontend Update

--- Anime Frontend/js/index.js
+++ Anime Frontend/js/index.js
@@ -17,4 +17,8 @@
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
       }
-      slides[slideIndex-1].style.display = "block";}
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 3000); // Change image every 2 seconds
+    }
