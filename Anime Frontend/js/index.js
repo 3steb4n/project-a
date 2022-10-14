e6900b62709d1f8f5494143cfd4b7b82431b9174
Tadokiari <78386551+Rielaph@users.noreply.github.com>
2022-10-14 19:59:34 -0400

    fix

--- Anime Frontend/js/index.js
+++ Anime Frontend/js/index.js
@@ -1,24 +1,19 @@
-    let slideIndex = 1;
-    showSlides(slideIndex);
-    
-    function nextslide(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("carousel");
-      if (n > slides.length) {slideIndex = 1}    
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 3000); // Change image every 2 seconds
-    }
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel");
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";
+}
